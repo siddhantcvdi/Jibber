f245f563808d8d39d0394ce2d96f383269db2e01
siddhantcvdi <dev.siddhantcvdi@gmail.com>
2025-11-05 20:35:59 +0530

    Deleted old server

--- jibber-backend/src/routes/messages.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import authMiddleware from '../middlewares/auth.middleware.js';
-import { getMessages } from '../controllers/messages.controller.js';
-const router = Router();
-
-router.route('/:chatId').get(authMiddleware, getMessages);
-
-export default router;
-
